6d4e96a3afc3ec52e80de83e584466fcea6c1d37
DOBIN <ff5026@naver.com>
2025-04-07 16:48:15 +0900

    [update] 프로젝트 리팩토링 04/07

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -3,32 +3,24 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 import { MainSeeder } from './seeds/main.seeder';
 
-const entitiesPath = process.env.NODE_ENV === 'test'
-    ? [__dirname + '/../**/*.entity{.ts,.js}']
-    : ['dist/**/*.entity{.js}'];
-
-const migrationsPath = process.env.NODE_ENV === 'test'
-    ? [__dirname + '/migrations/*{.ts,.js}']
-    : ['dist/db/migrations/*{.js}'];
-
-const seedsPath = process.env.NODE_ENV === 'test'
-    ? [__dirname + '/seeds/**/*{.ts,.js}']
-    : ['dist/db/seeds/**/*{.js}'];
+const entitiesPath = [__dirname + '/../../src/**/*.entity{.ts,.js}'];
+const migrationsPath = [__dirname + '/migrations/*{.ts,.js}'];
+const seedsPath = [__dirname + '/seeds/**/*{.ts,.js}'];
 
 export const dataSourceOptions: DataSourceOptions & SeederOptions = {
     type: 'mysql',
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '3306', 10),
-    username: process.env.DB_USERNAME || 'testuser',
+    port: parseInt(process.env.DB_PORT || '3307', 10),
+    username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || 'testpassword',
     database: process.env.DB_DATABASE || 'wave_deck',
     synchronize: false,
     logging: process.env.NODE_ENV === 'development',
     entities: entitiesPath,
     migrations: migrationsPath,
-    seeds: seedsPath,
+    seeds: [MainSeeder],
 };
 
 const dataSource = new DataSource(dataSourceOptions);
 
-export default dataSource; 
+export default dataSource;
